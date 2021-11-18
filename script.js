022a16515bc715efdbb87d64f249f59af9c13a73
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-11-18 23:59:25 +0000

    adds javascript render functions and saves inputs to local storage

---
+++ script.js
@@ -0,0 +1,57 @@
+var currentDate = moment();
+$("#currentDay").text(currentDate.format("dddd, MMMM Do"));
+
+let timeSlots = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12AM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+
+var container = document.querySelector(".container");
+
+timeSlots.forEach(function (value, index) {
+  let header = document.createElement("div");
+  let input = document.createElement("textarea");
+  // alterantivly could use input element
+  let button = document.createElement("button");
+  let timeblock = document.createElement("div");
+  header.setAttribute("class", "col hour");
+  input.setAttribute("class", "col-9 description");
+  input.setAttribute("name", index);
+  button.setAttribute("class", "col saveBtn");
+  button.setAttribute("name", index);
+  timeblock.setAttribute("class", "row time-block");
+  button.textContent = "Submit";
+  header.textContent = value;
+  timeblock.appendChild(header);
+  timeblock.appendChild(input);
+  timeblock.appendChild(button);
+  container.appendChild(timeblock);
+  button.addEventListener("click", handleFormSubmit);
+});
+
+let test = 0;
+let arr = [];
+
+// let scheduleItem = document.querySelector(`input[name="${test}"]`);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var element = event.target;
+  console.log(element.name);
+  // select form element by its `name` attribute and get its value
+  //   var inputEl = $('input[name="shopping-input"]');
+  let scheduleItem = document.querySelector(
+    `textarea[name="${element.name}"]`
+  ).value;
+  //add .value to the line above;
+  console.log(scheduleItem);
+  arr[element.name] = scheduleItem;
+  localStorage.setItem("scheduleArray", arr);
+}
